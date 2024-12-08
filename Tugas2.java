c00d1df8ee2557c17c7a7cf1b6705090017657e0
nevita <triyanevita7@gmail.com>
2024-12-08 23:03:50 +0700

    PenjumlahanRekursif

---
+++ Tugas2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Tugas2 {
+    public static int penjumlahanRekursif(int n) {
+        if (n == 1) {
+            return 1; 
+        }
+        return n + penjumlahanRekursif(n - 1); 
+    }
+
+    public static void main(String[] args) {
+        int f = 8; 
+        int hasil = penjumlahanRekursif(f);
+        System.out.println("Hasil penjumlahan dari 1 sampai " + f + " adalah: " + hasil);
+    }
+}
+
